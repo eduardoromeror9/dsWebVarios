8657166b22b8b5f62af9ba976edbf567621a9102
Eduardo Romero <eduardoromeror9@outlook.com>
2025-08-07 03:58:07 -0400

    readme pend proyecto web music

--- festivalMusica/src/js/app.js
+++ festivalMusica/src/js/app.js
@@ -1,9 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+  navegacionFija();
   crearGaleria();
-
+  resaltarEnlace();
+  scrollNav();
   
 });
+
+function navegacionFija() {
+  const header = document.querySelector('.header');
+  const sobreFestival = document.querySelector('.sobre-festival');
+
+  window.addEventListener('scroll', function() {
+    if (sobreFestival.getBoundingClientRect().top < 0) {
+      header.classList.add('fixed');
+    } else {
+      header.classList.remove('fixed');
+    }
+  }) 
+}
 
 function crearGaleria() {
 
@@ -53,4 +68,41 @@ function cerrarModal() {
     body.classList.remove('overflow-hidden');
     
   }, 500); // Espera 0.5 segundos antes de eliminar el modal
+}
+
+const resaltarEnlace = () => {
+  document.addEventListener("scroll", function () {
+    const sections = document.querySelectorAll("section");
+    const enlaces = document.querySelectorAll(".navegacion-principal a");
+
+    let actual = "";
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+
+      if (window.scrollY >= sectionTop - sectionHeight / 3) {
+        actual = section.id;
+      }
+    });
+    enlaces.forEach((link) => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === "#" + actual) {
+        link.classList.add("active");
+      }
+    });
+  });
+};
+
+function scrollNav() {
+  const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+  enlaces.forEach(enlace => {
+    enlace.addEventListener('click', function(e) {
+      e.preventDefault();
+      const seccion = document.querySelector(this.getAttribute('href'));
+      seccion.scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
 }
